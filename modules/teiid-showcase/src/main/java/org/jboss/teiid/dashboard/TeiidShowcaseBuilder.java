26022bc9a020edabc852f6a2f6c62fe3a657ea37
Van Halbert <vhalbert@redhat.com>
2014-10-15 21:13:50 -0500

    create new branch for DV 6.1

--- modules/teiid-showcase/src/main/java/org/jboss/teiid/dashboard/TeiidShowcaseBuilder.java
+++ modules/teiid-showcase/src/main/java/org/jboss/teiid/dashboard/TeiidShowcaseBuilder.java
@@ -15,21 +15,35 @@
  */
 package org.jboss.teiid.dashboard;
 
+import java.util.List;
+
+import org.jboss.dashboard.CoreServices;
 import org.jboss.dashboard.annotation.Priority;
 import org.jboss.dashboard.annotation.Startable;
 import org.jboss.dashboard.annotation.config.Config;
+import org.jboss.dashboard.database.DataSourceManager;
+import org.jboss.dashboard.database.JNDIDataSourceEntry;
+import org.jboss.dashboard.error.TechnicalError;
 import org.jboss.dashboard.factory.InitialModuleRegistry;
+//import org.jboss.dashboard.initialModule.InitialModuleRegistry;
 import org.jboss.dashboard.kpi.KPIInitialModule;
 import org.jboss.dashboard.workspace.export.ImportWorkspacesModule;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 /**
  * Class that builds the Teiid Dashboard Builder's showcase on startup.
  */
 @ApplicationScoped
 public class TeiidShowcaseBuilder implements Startable {
+	
+	private static final String TEIID_LOGGING_NAME = "TeiidLogging";
+	private static final String JNDI_PATH = "java:/teiid-log-ds";
+	private static final String TEST_QUERY = "SELECT * FROM COMMANDLOG where ID=1";
+
 
     @Inject
     private InitialModuleRegistry initialModuleRegistry;
@@ -39,12 +53,25 @@ public class TeiidShowcaseBuilder implements Startable {
 
     @Inject @Config("WEB-INF/etc/appdata/initialData/showcaseWorkspace.xml")
     private String workspaceFile;
+    
+    @Inject @Config("WEB-INF/etc/appdata/initialData/kpiLogging.xml")
+    private String loggingkpiFile;
+
+    @Inject @Config("WEB-INF/etc/appdata/initialData/loggingWorkspace.xml")
+    private String loggingworkspaceFile;
+   
+    private DataSourceManager dataSourceManager;
+
 
     public Priority getPriority() {
         return Priority.NORMAL;
     }
 
     public void start() throws Exception {
+    	dataSourceManager = CoreServices.lookup().getDataSourceManager();
+    	
+    	if (dataSourceManager==null) throw new TechnicalError("Program Error: DataSourceManager was not obtained in order to check for TeiidLogging", null);
+             	
         KPIInitialModule kpis = new KPIInitialModule();
         kpis.setName("org.jboss.teiid.dashboard.showcase.KPIs");
         kpis.setImportFile(kpiFile);
@@ -56,5 +83,47 @@ public class TeiidShowcaseBuilder implements Startable {
         workspace.setImportFile(workspaceFile);
         workspace.setVersion(1);
         initialModuleRegistry.registerInitialModule(workspace);
+        
+        // LOGGING Workspace and KPI
+    	boolean loggingExist = checkExist(JNDI_PATH);
+    	if (loggingExist) {
+	    	checkAndCreateDatasources();
+	    
+	
+	        KPIInitialModule lkpis = new KPIInitialModule();
+	        lkpis.setName("org.jboss.teiid.dashboard.showcase.LoggingKPIs");
+	        lkpis.setImportFile(loggingkpiFile);
+	        lkpis.setVersion(1);
+	        initialModuleRegistry.registerInitialModule(lkpis);
+	
+	        ImportWorkspacesModule lworkspace = new ImportWorkspacesModule();
+	        lworkspace.setName("org.jboss.teiid.dashboard.showcase.LoggingWorkspace");
+	        lworkspace.setImportFile(loggingworkspaceFile);
+	        lworkspace.setVersion(1);
+	        initialModuleRegistry.registerInitialModule(lworkspace);   	
+    	}
+    }
+    
+    protected void checkAndCreateDatasources() throws Exception {
+    	List<String> names = dataSourceManager.getDataSourceNames();
+    	if (names != null && names.contains(TEIID_LOGGING_NAME)) {
+    		
+    	} else {
+        	JNDIDataSourceEntry jndi = new JNDIDataSourceEntry();
+        	jndi.setJndiPath(JNDI_PATH);
+        	jndi.setTestQuery(TEST_QUERY);
+        	jndi.setName(TEIID_LOGGING_NAME);
+        	
+        	jndi.save();
+        }
+    } 
+    
+    private boolean checkExist(String jndi) {
+   	 try {
+			InitialContext ctx = new InitialContext();  
+			return (ctx.lookup(jndi) != null ? true : false); //$NON-NLS-1$
+		} catch (NamingException e) {
+		}      	
+		return false;
     }
 }
